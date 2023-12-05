be21c35f76ff297fed8c769d38d6079bd191fbd7
Abdurrahman Rajab <rajab-2@windowslive.com>
2023-12-06 01:19:38 +0300

    feat: add text area parsing logic

--- content-script.js
+++ content-script.js
@@ -4,10 +4,31 @@ const textareas = document.querySelectorAll('textarea');
 // loop through all text areas
 textareas.forEach((textarea) => {
   textarea.addEventListener("input", (event) => {
+
     // get the text area value
     const textAreaValue = event.target.value;
+
     // check if latest element is : 
-    const lastElement = textAreaValue[textAreaValue.length - 1];
-    if (lastElement === ":") console.log("emoji time")
+    const lastElement = textAreaValue.lastIndexOf(":");
+    // Check if ":" is found in the string
+    if (lastElement !== -1) {
+      // Extract the text after ":"
+      const textArray = textAreaValue.substring(lastElement + 1).trim().split(" ");
+      if (textArray.length > 1 || textArray[0] === "") {
+        // chrome.runtime.sendMessage(
+        //   { type: 'remove-emoji'}, response => {
+        //     console.log(response)
+        // })
+        const unlistedItems = document.getElementById('chrome-extension-unlisted-items');
+        if (unlistedItems) unlistedItems.remove();
+        return;
+      }
+      let latestText = textArray[0];
+      console.log(latestText)
+      chrome.runtime.sendMessage(
+        { type: 'emoji', query: latestText }, response => {
+          console.log(response)
+        })
+    }
   });
 });
